fc3dbdc82feb584a925befb6bf8d1098de53e1c6
Joshua Magee <magee.joshua@yahoo.com>
2018-05-17 23:14:33 -0800

    Strip leading components to properly catch missing files included from different directories.
    For example, diagnose the case where we have:
     src/lib/foo.c
    
     That includes "foo.h", where foo.h is located in src/include/foo.h, and
     found via -Isrc/include/.
    
     This catches a fairly obvious pattern that was previously missed,
     although it could increase false positives.

--- test.sh
+++ test.sh
@@ -30,6 +30,12 @@ create_file() {
   echo $contents > $test_repo/$filename
 }
 
+create_dir() {
+  dirname=$1
+
+  mkdir -p $test_repo/$dirname
+}
+
 add_file() {
   filename=$1
   pushd $test_repo
@@ -43,6 +49,13 @@ commit() {
   ret=$?
   popd
   return $ret
+}
+
+clean() {
+  pushd $test_repo
+  git reset > /dev/null 2>&1
+  git clean -f > /dev/null 2>&1
+  popd
 }
 
 reset() {
@@ -78,27 +91,44 @@ install_hook
 
 echo "CHECK: Running a positive sanity test"
 
-create_file foo.c "#include \"foo.h\""
-create_file foo.h "static int x;"
+create_file foo1.c "#include \"foo1.h\""
+create_file foo1.h "static int x;"
 
-add_file foo.c
+add_file foo1.c
 commit
 result=$?
 
 expect_failure $result
 overall_result=$(($overall_result || $?))
+clean
 
 echo ------------------------
 echo "CHECK: Running a negative sanity test"
-create_file foo.c "#include \"bar.h\""
-create_file foo.h "static int x;"
+create_file foo2.c "#include \"bar.h\""
+create_file foo2.h "static int x;"
 
-add_file foo.c
+add_file foo2.c
 
 commit
 result=$?
 expect_success $result
 overall_result=$(($overall_result || $?))
+clean
+
+echo "CHECK: Running a nested file test"
+
+create_dir dir/a
+create_dir dir/b
+create_file dir/a/foo3.c "#include \"foo3.h\""
+create_file dir/b/foo3.h "static int x;"
+
+add_file dir/a/foo3.c
+commit
+result=$?
+
+expect_failure $result
+overall_result=$(($overall_result || $?))
+clean
 
 destroy_test_repo
 
